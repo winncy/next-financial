df4e78fb28f53620ecda8f3c2028d1479f39d2a5
lilin <lilin@dev.com>
2025-04-18 15:05:26 +0800

    样式

--- src/component/SuggestBox/index.tsx
+++ src/component/SuggestBox/index.tsx
@@ -5,9 +5,9 @@ export type SuggestBoxProps = {
 
 const SuggestBox = ({ title, description }: SuggestBoxProps) => {
   return (
-    <div className="flex h-fit w-fit min-w-0 cursor-pointer flex-col gap-2 rounded-lg border border-gray-100 bg-white px-8 py-4 text-[#112D4E]/60 shadow select-none hover:border-blue-100 hover:bg-blue-100 hover:text-[#112D4E]">
+    <div className="flex h-fit w-fit max-w-[320px] min-w-0 cursor-pointer flex-col gap-2 rounded-lg border border-gray-100 bg-white px-8 py-4 text-[#112D4E]/60 shadow select-none hover:border-blue-100 hover:bg-blue-100 hover:text-[#112D4E]">
       <div className="text-lg text-[#112D4E]">{title}</div>
-      <div className="flex flex-col gap-1 text-xs">
+      <div className="flex flex-col gap-2 text-xs">
         {description.map((desc, index) => (
           <li key={title + index}>{desc}</li>
         ))}
